8c5164abd8a1141e9f85004a54cff0e792a4933d
Jon Forsyth <jon@graphdatadesign.com>
2018-07-25 08:41:37 -0600

    Fail admin page test

---
+++ JUL_26_2018/sms/test/sms_web/views/admin_view_test.exs
@@ -0,0 +1,12 @@
+defmodule SmsWeb.AdminViewTest do
+  use SmsWeb.ConnCase, async: true
+  
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders admin.html" do
+    assert render_to_string(SmsWeb.AdminView, "admin.html", []) =~ ~r/Admin Page/
+
+  end
+
+end
