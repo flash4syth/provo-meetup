aa8fe7a75b3a7299b9f137aea101d4d6a1342a7a
Jon Forsyth <jon@graphdatadesign.com>
2018-07-26 15:19:50 -0600

    Refactor hrefs, query number lists

--- JUL_26_2018/sms/lib/sms_web/controllers/admin_controller.ex
+++ JUL_26_2018/sms/lib/sms_web/controllers/admin_controller.ex
@@ -1,12 +1,24 @@
 defmodule SmsWeb.AdminController do
   use SmsWeb, :controller
+  alias Sms.Users
 
   def admin(conn, params) do
 
-    conn = assign(conn, :selected_list, params["list"])
-    
+    number_list =
+      case params["list"] do
+        "registered" -> Users.registered_sms_users()
+        "stop" -> Users.stop_sms_users()
+        "requests" -> Users.requested_sms_users()
+        "admin" -> Users.admin_sms_users()
+        nil -> Users.registered_sms_users()
+      end
+
     # remove default phoenix layout
-    conn = put_layout(conn, "admin_layout.html")
+    conn =
+      put_layout(conn, "admin_layout.html")
+      |> assign(:list_type, params["list"])
+      |> assign(:number_list, number_list)
+
 
     render(conn, "admin.html")
   end
--- JUL_26_2018/sms/test/sms_web/controllers/admin_controller_test.exs
+++ JUL_26_2018/sms/test/sms_web/controllers/admin_controller_test.exs
@@ -2,32 +2,32 @@ defmodule SmsWeb.AdminControllerTest do
   use SmsWeb.ConnCase
 
   test "GET /admin", %{conn: conn} do
-    conn = get conn, "/admin"
+    conn = get(conn, "/admin")
     assert html_response(conn, 200) =~ "Admin Page"
   end
 
-  test "GET /admin/registered" do
-    conn = get conn, "/admin/registered"
+  test "GET /admin/registered", %{conn: conn} do
+    conn = get(conn, "/admin/registered")
     assert html_response(conn, 200) =~ "Registered Numbers"
   end
 
-  test "GET /admin/requests" do
-    conn = get conn, "/admin/requests"
+  test "GET /admin/requests", %{conn: conn} do
+    conn = get(conn, "/admin/requests")
     assert html_response(conn, 200) =~ "Subscription Requests"
   end
 
-  test "GET /admin/stop" do
-    conn = get conn, "/admin/stop"
+  test "GET /admin/stop", %{conn: conn} do
+    conn = get(conn, "/admin/stop")
     assert html_response(conn, 200) =~ "Stop Numbers"
   end
 
-  test "GET /admin/admin" do
-    conn = get conn, "/admin/admin"
+  test "GET /admin/admin", %{conn: conn} do
+    conn = get(conn, "/admin/admin")
     assert html_response(conn, 200) =~ "Admin Numbers"
   end
 
-  test "GET /admin/twilio" do
-    conn = get conn, "/admin/twilio"
-    assert html_response(conn, 200) =~ "Twilio Numbers"
-  end
+  # test "GET /admin/twilio", %{conn: conn} do
+  #   conn = get(conn, "/admin/twilio")
+  #   assert html_response(conn, 200) =~ "Twilio Numbers"
+  # end
 end
