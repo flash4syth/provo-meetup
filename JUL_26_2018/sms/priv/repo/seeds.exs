0196ee3e932ed43537f4a843e4c4ea9251fb87ad
Jon Forsyth <jon@graphdatadesign.com>
2018-07-26 14:06:20 -0600

    Add sms_users via seeds.exs

--- JUL_26_2018/sms/priv/repo/seeds.exs
+++ JUL_26_2018/sms/priv/repo/seeds.exs
@@ -9,3 +9,17 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Sms.Repo
+alias Sms.Users.SmsUser
+
+[
+  %SmsUser{number: "555-123-4567", status: :registered},
+  %SmsUser{number: "555-123-4567", status: :registered},
+  %SmsUser{number: "555-123-4567", status: :registered},
+  %SmsUser{number: "555-123-4567", status: :registered, is_admin: true},
+  %SmsUser{number: "555-123-4567", status: :stop},
+  %SmsUser{number: "555-123-4567", status: :stop},
+  %SmsUser{number: "555-123-4567", status: :request_pending},
+  %SmsUser{number: "555-123-4567", status: :request_pending},
+]
+|> Enum.each(fn(sms_user)-> Repo.insert!(sms_user) end)
